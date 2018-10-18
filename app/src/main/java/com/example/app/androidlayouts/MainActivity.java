d2de6be06f2416e6bdf95f3c6f3b4846a638bac8
Muthuraja <muthuraja.g@newgendigital.com>
2018-10-18 14:37:31 +0530

    Few more Layouts added

--- app/src/main/java/com/example/app/androidlayouts/MainActivity.java
+++ app/src/main/java/com/example/app/androidlayouts/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.app.androidlayouts;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void goNextScreen(View v){
+        startActivity(new Intent(MainActivity.this, RelativeActivity.class));
     }
 }
